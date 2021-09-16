467f08cdc6279a24be7c1710d61dd58b62bcd01a
Moehau <binh@autonomous.nyc>
2021-09-16 17:24:36 +0700

    Delete testwifi.py

--- aos/ability/product_control/testwifi.py
+++
@@ -1,4 +0,0 @@
-from aos.system.configs.channel import DEVICE_TYPE
-from aos.system.libs.util import Util
-Util.cmd("sudo nmcli c down Hotspot",True)
-Util.cmd("sudo nmcli d wifi connect Autonomous password @11235813",True)
